ae4be49e205b1ae735f8472c55e6e459d32617fe
James Reeves <jreeves@weavejester.com>
2013-07-18 22:42:52 +0100

    Added vector lookup and equality

--- src/euclidean/math/vector.clj
+++ src/euclidean/math/vector.clj
@@ -2,8 +2,23 @@
   (:refer-clojure :exclude [vector]))
 
 (deftype Vector3D [x y z]
+  clojure.lang.ILookup
+  (valAt [v i]
+    (.valAt v i nil))
+  (valAt [_ i not-found]
+    (case i 0 x 1 y 2 z not-found))
+
+  clojure.lang.IFn
+  (invoke [v i]
+    (.valAt v i))
+
   Object
-  (toString [_] (str "#math/vector [" x " " y " " z "]")))
+  (toString [_]
+    (str "#math/vector [" x " " y " " z "]"))
+  (equals [_ v]
+    (and (= x (v 0))
+         (= y (v 1))
+         (= z (v 2)))))
 
 (defn vector [x y z]
   (Vector3D. x y z))
--- test/euclidean/math/vector_test.clj
+++ test/euclidean/math/vector_test.clj
@@ -5,3 +5,12 @@
 (deftest test-data-readers
   (is (= (pr-str (v/vector 1 2 3))
          "#math/vector [1 2 3]")))
+
+(deftest test-equality
+  (is (= (v/vector 1 2 3) (v/vector 1 2 3)))
+  (is (not= (v/vector 1 2 3) (v/vector 1 2 4))))
+
+(deftest test-lookup
+  (let [v (v/vector 1 2 3)]
+    (is (= (get v 0) 1))
+    (is (= (v 1) 2))))
