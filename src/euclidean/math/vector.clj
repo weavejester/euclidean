40a39ef82c3c7106c6c28ba67bb2fa57025b630b
James Reeves <jreeves@weavejester.com>
2013-07-19 21:32:58 +0100

    Added 2D vector type

--- src/euclidean/math/vector.clj
+++ src/euclidean/math/vector.clj
@@ -4,6 +4,48 @@
 (defprotocol Vector
   (add [v1 v2]))
 
+(definterface Coords2D
+  (^double getX [])
+  (^double getY []))
+
+(defn- add-2d [^Vector2D v1 ^Vector2D v2]
+  (Vector2D. (+ (.getX v1) (.getX v2))
+             (+ (.getY v1) (.getY v2))))
+
+(deftype Vector2D [^double x ^double y]
+  Coords2D
+  (getX [_] x)
+  (getY [_] y)
+
+  Vector
+  (add [v1 v2] (add-2d v1 v2))
+
+  clojure.lang.Counted
+  (count [_] 2)
+
+  clojure.lang.Sequential
+
+  clojure.lang.Seqable
+  (seq [_] (list x y))
+
+  clojure.lang.ILookup
+  (valAt [v i]
+    (.valAt v i nil))
+  (valAt [_ i not-found]
+    (case i 0 x 1 y not-found))
+
+  clojure.lang.IFn
+  (invoke [v i]
+    (.valAt v i))
+
+  Object
+  (toString [_]
+    (str "#math/vector [" x " " y "]"))
+  (equals [_ v]
+    (and (= (count v) 2)
+         (= x (v 0))
+         (= y (v 1)))))
+
 (definterface Coords3D
   (^double getX [])
   (^double getY [])
@@ -45,15 +87,22 @@
   (toString [_]
     (str "#math/vector [" x " " y " " z "]"))
   (equals [_ v]
-    (and (= x (v 0))
+    (and (= (count v) 3)
+         (= x (v 0))
          (= y (v 1))
          (= z (v 2)))))
 
-(defn vector [^double x ^double y ^double z]
-  (Vector3D. x y z))
+(defn vector
+  ([^double x ^double y]
+     (Vector2D. x y))
+  ([^double x ^double y ^double z]
+     (Vector3D. x y z)))
 
 (defn into-vector [v]
   (apply vector v))
+
+(defmethod print-method Vector2D [v ^java.io.Writer w]
+  (.write w (.toString v)))
 
 (defmethod print-method Vector3D [v ^java.io.Writer w]
   (.write w (.toString v)))
