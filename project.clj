b9d0411ba07356fe4db1ab7e271b56f4897ad90a
James Reeves <jreeves@weavejester.com>
2013-07-23 21:14:30 +0100

    Filled out URL and description in project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euclidean "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for fast, immutable 3D math"
+  :url "https://github.com/weavejester/euclidean"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
