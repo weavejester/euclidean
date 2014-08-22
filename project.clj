057b2035d3240a0e404f65ad24d312d12ef8d83d
James Reeves <jreeves@weavejester.com>
2014-08-22 18:16:29 +0100

    Update Codox dependency to 0.8.10

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[codox "0.6.4"]]  
+  :plugins [[codox "0.8.10"]]  
   :profiles
   {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                         [org.clojure/test.check "0.5.9"]
